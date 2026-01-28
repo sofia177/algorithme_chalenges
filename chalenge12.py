940a968ddd0e899d2f0d7a54b11c37cef9278a8a
sofia177 <sofialalagui6@gmail.com>
2026-01-28 20:49:14 +0100

    Initial commit

---
+++ chalenge12.py
@@ -0,0 +1,10 @@
+from itertools import permutations
+
+def getPermutation(n, k):
+    nums = [str(i) for i in range(1, n + 1)]
+    perms = list(permutations(nums))
+    perms.sort()
+    return "".join(perms[k - 1])
+print(getPermutation(4, 8))  
+
+#the itertools.permutations function generates all possible arrangements of the input elements.
